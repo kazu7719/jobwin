d4e0426186ce7c402c7860eb78489828bd20316d
kazu7719 <kazuki.eripon7719@icloud.com>
2025-11-18 16:00:08 +0900

    カレンダー表示(トップページ)のフロント・バックエンド作成

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,22 @@
 class CalendarsController < ApplicationController
   def index
+    @today = Date.current
+    @month_date = requested_month || @today.beginning_of_month
+    @month_label = @month_date.strftime("%Y年%-m月")
+    @days_in_month = @month_date.end_of_month.day
+    @leading_blank_cells = @month_date.wday
+    @total_cells = ((@leading_blank_cells + @days_in_month) / 7.0).ceil * 7
+    @prev_month_param = (@month_date - 1.month).strftime("%Y-%m")
+    @next_month_param = (@month_date + 1.month).strftime("%Y-%m")
+  end
+
+  private
+
+  def requested_month
+    return if params[:month].blank?
+
+    Date.strptime(params[:month], "%Y-%m").beginning_of_month
+  rescue ArgumentError
+    nil
   end
 end
