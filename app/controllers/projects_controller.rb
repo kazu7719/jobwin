a963c6c78cf52dc4314485d219fd0af2ae15ecc4
kazu7719 <kazuki.eripon7719@icloud.com>
2025-12-04 08:09:51 +0900

    プロジェクト新規登録画面の実装

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,8 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.includes(:user).order('created_at DESC')
   end
+
+  def new
+    @project = Project.new
+  end
 end
