d8b8e1575de93850ac34e80d2c23482ebec6f4d2
kazu7719 <kazuki.eripon7719@icloud.com>
2025-12-06 11:12:12 +0900

    タスク詳細・編集・削除機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:show,:edit,:update,:destroy]
 
   def index
     @tasks = Task.includes(:user).order('created_at DESC')
@@ -18,10 +19,36 @@ class TasksController < ApplicationController
     end
   end
 
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:task_name, :start_day, :schedule_end_day, :end_day, :memo).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @task = Task.find(params[:id])
   end
 
 end
