c03115518081e9611d7033d3a6c55cb349825ef9
kazu7719 <kazuki.eripon7719@icloud.com>
2025-12-07 09:53:04 +0900

    カレンダーの修正①

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,9 +8,51 @@ class CalendarsController < ApplicationController
     @total_cells = ((@leading_blank_cells + @days_in_month) / 7.0).ceil * 7
     @prev_month_param = (@month_date - 1.month).strftime("%Y-%m")
     @next_month_param = (@month_date + 1.month).strftime("%Y-%m")
+
+
+    @calendar_entries = Hash.new { |hash, date| hash[date] = { projects: [], tasks: [], habits: [] } }
+
+    build_calendar_entries if user_signed_in?
   end
 
   private
+
+  def build_calendar_entries
+    month_range = @month_date.beginning_of_month..@month_date.end_of_month
+
+    current_user.projects.find_each do |project|
+      add_span_entry(month_range, project.start_day, project.schedule_end_day, :projects, project.project_name)
+    end
+
+    Task.where(user_id: current_user.id).find_each do |task|
+      add_span_entry(month_range, task.start_day, task.schedule_end_day, :tasks, task.task_name)
+    end
+
+    Habit.where(user_id: current_user.id).find_each do |habit|
+      add_single_entry(month_range, habit.start_day, :habits, habit.habit_name)
+    end
+  end
+
+  def add_span_entry(month_range, start_day, end_day, type_key, label)
+    return if start_day.blank? || end_day.blank?
+
+    range_start = [start_day.to_date, month_range.first].max
+    range_end = [end_day.to_date, month_range.last].min
+    return if range_end < range_start
+
+    (range_start..range_end).each do |date|
+      @calendar_entries[date][type_key] << label
+    end
+  end
+
+  def add_single_entry(month_range, day, type_key, label)
+    return if day.blank?
+
+    date = day.to_date
+    return unless month_range.cover?(date)
+
+    @calendar_entries[date][type_key] << label
+  end
 
   def requested_month
     return if params[:month].blank?
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
 
   has_many :projects
   has_many :project_tasks
+  has_many :tasks
+  has_many :habits
+  
 end
